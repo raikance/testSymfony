8034f7ffb3c7fc711da5f7ddb532f5ed43b0993e
Coraline Chau <coraline.chau@gmail.com>
2022-05-02 17:04:56 +0200

    Fin journée test twig

---
+++ src/Controller/TroisièmeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TroisièmeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+
+    public function buttonToTroisieme
+    {
+
+    }
+}
