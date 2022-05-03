298f8a32a8417370129fcb92051488a2f702700c
coco <coraline.chau@gmail.com>
2022-05-03 18:32:42 +0200

    réparation de mes bêtises

--- src/Controller/TroisièmeController.php
+++ src/Controller/TroisièmeController.php
@@ -8,11 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TroisièmeController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("App/Controller", name="trois")
      */
 
-    public function buttonToTroisieme
+    public function buttonToTroisieme($trois)
     {
-
+        return $this->render("troisième.html.twig",['trois' => $trois]);
     }
 }
